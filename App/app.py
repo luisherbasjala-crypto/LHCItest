a1cbc1d979df5dc5791c9e972ceaaffeea70c8a5
luisherbasjala-crypto <luisherbasjala@gmail.com>
2025-09-24 17:37:11 -0400

    Create app.py
    Create the app.py

---
+++ App/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+# Crea una instancia de la aplicación Flask
+app = Flask(__name__)
+
+
+# Define una ruta para la página principal
+@app.route("/")
+def Tarea_Python():
+    return "¡Mensaje para la tarea!"
+	
+
+# Ejecuta la aplicación si este script es el principal
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5500)
+    
